a95d2a9e2b695d432c6d1a971c40c582368fa1f0
EquipeHerz <dev@equipherz.com>
2026-03-03 17:17:04 -0300

    fix: merge changes and apply stash

--- src/components/ui/input.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { StyledInput } from "@/styles/components/Input";
-
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(({ type, ...props }, ref) => {
-  return <StyledInput type={type} ref={ref} {...props} />;
-});
-Input.displayName = "Input";
-
-export { Input };
